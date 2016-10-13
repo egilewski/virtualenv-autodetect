03b6ca2c702c336b5466b45d40f4895bd6166284
Robert DeRose <RobertDeRose@gmail.com>
2016-10-13 12:22:59 -0400

    Add support for macOS

--- virtualenv-autodetect.sh
+++ virtualenv-autodetect.sh
@@ -1,5 +1,5 @@
 # virtualenv-autodetect.sh
-# 
+#
 # Installation:
 # Add this line to your .zshenv, .bashrc or .bash-profile:
 #
@@ -7,17 +7,16 @@
 
 # https://github.com/egilewski/virtualenv-autodetect
 
-_VIRTUALENV_ACTIVATION_SCRIPT_PATH="bin/activate"
 
 _virtualenv_auto_activate() {
     _virtualenv_path=$(_get_virtualenv_path)
     if [ -e "$_virtualenv_path" ]
     then
         # Check if already activated to avoid redundant activation.
-        if [ "$VIRTUAL_ENV" != $(readlink -f "$_virtualenv_path") ]
+        if [ "$VIRTUAL_ENV" != "$_virtualenv_path" ]
         then
             VIRTUAL_ENV_DISABLE_PROMPT=1
-            source "$_virtualenv_path/$_VIRTUALENV_ACTIVATION_SCRIPT_PATH"
+            source "$_virtualenv_path"
         fi
     else
         deactivate 2>/dev/null
@@ -28,23 +27,16 @@ _virtualenv_auto_activate() {
 _get_virtualenv_path() {
     # Find subdir of given one that is a virtualenv dir.
     _find_virtualenv_subdir() {
-        for i in $(find $1 -maxdepth 1 -printf %f\\n)
-        do
-            if [ -e "$1/$i/$_VIRTUALENV_ACTIVATION_SCRIPT_PATH" ]
-            then
-                echo "$i"
-                break
-            fi
-        done
+        find $1 -maxdepth 2 -type d -name 'bin' -exec find {} -name 'activate' \; 2> /dev/null
     }
 
     _current_dir="$PWD"
     while true
     do
         _virtualenv_subdir=$(_find_virtualenv_subdir "$_current_dir")
-        if [ "$_virtualenv_subdir" ]
+        if [ -n "$_virtualenv_subdir" ]
         then
-            echo "$_current_dir/$_virtualenv_subdir"
+            echo "$_virtualenv_subdir"
             return
         else
             if [[ "$_current_dir" != "/" ]]
